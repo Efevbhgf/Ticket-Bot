8b0470464ed38087d6d1a5078016c716456cd2c7
Efevbhgf <efemustafa190777@gmail.com>
2025-04-19 09:51:58 +0300

    Update ayarlar.js

--- ayarlar.js
+++ ayarlar.js
@@ -1,10 +1,10 @@
 module.exports = {
-     botdurum: "DURUM YAZI",
-     ticketkanal: "TİCKET KANAL ID",
-     ticketkategori: "TİCKET KATEGORİ ID",
-     ticketlog: "TİCKET LOG ID",
-     destekemoji: "DESTEK BUTON EMOJİ>",
-     destekkapatemoji: "DESTEK KAPAT BUTON EMOJİ",
-     yetkili: "YETKİLİ ROL ID",
-     token: "TOKEN",
+     botdurum: "/godomanisiktim1234",
+     ticketkanal: "1362817874217140538",
+     ticketkategori: "1362817874217140536",
+     ticketlog: "1362817907951931712",
+     destekemoji: "<:emoji_1:1362818301842952213>",
+     destekkapatemoji: "<:emoji_1:1362818301842952213",
+     yetkili: "1362817960204308490",
+     token: "MTM2MzA0NDEzMjkyMjMzMTE4Nw.GWh2UL.3PzAVu9J2ivcSetTqpt6XBRk-29AmkR7AqDDmY",
 }
